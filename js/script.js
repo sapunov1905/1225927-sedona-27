9c24c3e446ed8dc4dc542de27003475d6169b941
Александр <kenas1997@rambler.ru>
2020-03-22 18:19:34 +0300

    Введение в JavaScript

---
+++ js/script.js
@@ -0,0 +1,47 @@
+var link = document.querySelector(".search-button-map");
+var popup = document.querySelector(".form");
+
+var form = popup.querySelector("form");
+var date_in = popup.querySelector("[name=date_in]");
+var date_out = popup.querySelector("[name=date_out]");
+var adults = popup.querySelector("[name=adults]");
+var kids = popup.querySelector("[name=kids]");
+
+var isStorageSupport;
+var adultsStorage = "";
+var kidsStorage = "";
+
+try {
+  adultsStorage = localStorage.getItem("adults");
+  kidsStorage = localStorage.getItem("kids");
+  isStorageSupport = true;
+} catch (err) {
+  isStorageSupport = false;
+}
+
+if (isStorageSupport) {
+  adults.value = adultsStorage;
+  kids.value = kidsStorage;
+}
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.toggle("form-hide");
+  date_in.focus();
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!date_in.value
+    || !date_out.value
+    || !adults.value
+    || !kids.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+    console.log("Нужно заполнить все поля");
+  } else {
+    localStorage.setItem("adults", adults.value);
+    localStorage.setItem("kids", kids.value);
+  }
+});
